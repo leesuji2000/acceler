92f07f19f4f02484deb8625cdd6ec01774f52117
leesuji <qaz8457@naver.com>
2023-11-22 21:26:53 +0900

    accler1

--- App.js
+++ App.js
@@ -1,20 +1,148 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Accelerometer } from 'expo-sensors';
+import { LineChart } from 'react-native-chart-kit';
 
 export default function App() {
+  const [data, setData] = useState({
+    x: [],
+    y: [],
+    z: [],
+  });
+  const [subscription, setSubscription] = useState(null);
+
+  const _slow = () => Accelerometer.setUpdateInterval(1000);
+  const _fast = () => Accelerometer.setUpdateInterval(16);
+
+  const _subscribe = () => {
+    setSubscription(Accelerometer.addListener(handleAccelerometerChange));
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  const handleAccelerometerChange = ({ x, y, z }) => {
+    setData((prevData) => ({
+      x: [...prevData.x, x],
+      y: [...prevData.y, y],
+      z: [...prevData.z, z],
+    }));
+  };
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Accelerometer: (in gs where 1g = 9.81 m/s^2)</Text>
+      <Text style={styles.text}>x: {data.x[data.x.length - 1]}</Text>
+      <Text style={styles.text}>y: {data.y[data.y.length - 1]}</Text>
+      <Text style={styles.text}>z: {data.z[data.z.length - 1]}</Text>
+      <View style={styles.chartContainer}>
+        <LineChart
+          data={{
+            labels: Array.from({ length: data.x.length }, (_, i) => i.toString()),
+            datasets: [
+              {
+                data: data.x,
+                color: (opacity = 1) => `rgba(255, 0, 0, ${opacity})`,
+              },
+            ],
+          }}
+          width={300}
+          height={200}
+          yAxisLabel="X"
+          yAxisSuffix=""
+          chartConfig={chartConfig}
+        />
+        <LineChart
+          data={{
+            labels: Array.from({ length: data.y.length }, (_, i) => i.toString()),
+            datasets: [
+              {
+                data: data.y,
+                color: (opacity = 1) => `rgba(0, 255, 0, ${opacity})`,
+              },
+            ],
+          }}
+          width={300}
+          height={200}
+          yAxisLabel="Y"
+          yAxisSuffix=""
+          chartConfig={chartConfig}
+        />
+        <LineChart
+          data={{
+            labels: Array.from({ length: data.z.length }, (_, i) => i.toString()),
+            datasets: [
+              {
+                data: data.z,
+                color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`,
+              },
+            ],
+          }}
+          width={300}
+          height={200}
+          yAxisLabel="Z"
+          yAxisSuffix=""
+          chartConfig={chartConfig}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
+          <Text>{subscription ? 'On' : 'Off'}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
+          <Text>Slow</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_fast} style={styles.button}>
+          <Text>Fast</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
+const chartConfig = {
+  backgroundGradientFrom: '#fff',
+  backgroundGradientTo: '#fff',
+  color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+  style: {
+    borderRadius: 16,
+  },
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  text: {
+    textAlign: 'center',
+  },
+  chartContainer: {
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    marginTop: 15,
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eee',
+    padding: 10,
+  },
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: '#ccc',
   },
 });
